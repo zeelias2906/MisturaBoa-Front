d362e1104e1b6948a562e89b4a011b9375249426
zeelias2906 <js.elias27@hotmail.com>
2025-05-05 15:02:59 -0300

    Resolvendo conflitos00

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    url: 'https://mistura-boa-api.onrender.com/', // URL do back-end em produção
+    url: 'https://mistura-boa-api.onrender.com', // URL do back-end em produção
     DEFAULT_MODAL_OPTIONS: {
         centered: true,
     },
